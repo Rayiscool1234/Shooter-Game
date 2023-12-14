f2975df3330780fe2a8d793ce3579318d93b8027
Rayiscool1234 <rayan.almoabadi@gmail.com>
2023-12-14 19:04:17 +0300

    Added sprinting Modified Controls

--- Shooter/main.cpp
+++ Shooter/main.cpp
@@ -22,6 +22,8 @@ class Player {
 public:
     sf::RectangleShape shape;
     float speed{ 250.0f }; // Speed in pixels per second
+    float stamina{ 10.0f }; // stamina points
+    bool isrunning{ false };
     Player(float x, float y) {
         shape.setSize(sf::Vector2f(50.0f, 50.0f));
         shape.setFillColor(sf::Color::Red);
@@ -29,10 +31,12 @@ public:
     }
 
     void update(float deltaTime) {
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) shape.move(0, -speed * deltaTime);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) shape.move(0, speed * deltaTime);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) shape.move(-speed * deltaTime, 0);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) shape.move(speed * deltaTime, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) shape.move(0, -speed * deltaTime);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) shape.move(0, speed * deltaTime);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) shape.move(-speed * deltaTime, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) shape.move(speed * deltaTime, 0);
+        if ( stamina != 0.0 && !isrunning && sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift)) { speed = speed * 2; isrunning = true; }
+        else { speed = 250.0f; isrunning = false; }
     }
 };
 
@@ -66,7 +70,7 @@ public:
         shape.setPosition(x, y);
 
         // Calculate rotation
-        float rotation = atan2(direction.y, direction.x) * 180 / 3.14159265;
+        float rotation = static_cast<float>(atan2(direction.y, direction.x) * 180 / 3.14159265);
         shape.setRotation(rotation);
     }
 
@@ -113,7 +117,7 @@ int WinMain() {
         float magnitude = std::sqrt(direction.x * direction.x + direction.y * direction.y);
         direction /= magnitude; // Normalize the direction vector
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && Reload(clockBullets.getElapsedTime().asMilliseconds())) {
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && Reload(clockBullets.getElapsedTime().asMilliseconds())) {
             bullets.push_back(Bullet(playerPos.x + 25, playerPos.y + 25, direction)); // Adjust bullet spawn position if needed
             clockBullets.restart();
         }
